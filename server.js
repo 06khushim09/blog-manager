d99153865daa84685007a50e439af5d46ca932be
06khushim09 <arth47766@gmail.com>
2026-07-24 10:49:00 +0530

    Day 5: Added Express server with GET and POST routes

--- server.js
+++ server.js
@@ -2,12 +2,45 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = 3000;
 
+// Home Route
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Welcome to Blog Manager Backend!");
+});
+
+// GET Blogs Route
+app.get("/blogs", (req, res) => {
+    const blogs = [
+        {
+            id: 1,
+            title: "My First Blog"
+        },
+        {
+            id: 2,
+            title: "Learning Express"
+        }
+    ];
+
+    res.json(blogs);
+});
+
+// POST Blog Route
+app.post("/blogs", (req, res) => {
+
+    const newBlog = req.body;
+
+    console.log(newBlog);
+
+    res.json({
+        message: "Blog received successfully!",
+        blog: newBlog
+    });
+
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
